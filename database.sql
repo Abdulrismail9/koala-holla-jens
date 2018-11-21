c0c0e9b004ddc0ebc3303a20938326ee13a0ea7c
Abdul  Ismail <abdulismail@Abduls-MacBook-Air.local>
2018-11-21 10:16:20 -0600

    Added more koalas to database

--- database.sql
+++ database.sql
@@ -11,3 +11,18 @@ CREATE TABLE koalas(
 
 INSERT INTO koalas( name, gender, age, ready_to_transfer, notes)
 VALUES ('Jean', 'female', '5', 'true', 'allergic to lots of lava');
+
+INSERT INTO koalas( name, gender, age, ready_to_transfer, notes)
+VALUES ('Scotty', 'male', '4', 'true', 'Born in Guatamala');
+
+INSERT INTO koalas( name, gender, age, ready_to_transfer, notes)
+VALUES ('Ororo', 'female', '7', 'false', 'Loves listening to Paula (Abdul)');
+
+INSERT INTO koalas( name, gender, age, ready_to_transfer, notes)
+VALUES ('Logan', 'male', '15', 'false', 'Loves the sauna');
+
+INSERT INTO koalas( name, gender, age, ready_to_transfer, notes)
+VALUES ('Charlie', 'male', '9', 'true', 'Favorite band is Nirvana');
+
+INSERT INTO koalas( name, gender, age, ready_to_transfer, notes)
+VALUES ('Betsy', 'female', '4', 'true', 'Has a pet iguana');
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -38,3 +38,4 @@ function saveKoala( newKoala ){
   // ajax call to server to get koalas
  
 }
+

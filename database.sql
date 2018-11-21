3b631c565afcfd94dccbbba412cbc06cb74a973a
Travis Lang <tlang50@icloud.com>
2018-11-21 10:05:21 -0600

    add to db file

--- database.sql
+++ database.sql
@@ -1,8 +1,13 @@
-CREATE TABLE books(
-	id SERIAL PRIMARY KEY,
-	name VARCHAR (250) NOT NULL,
-	gender VARCHAR (100) NOT NULL,
-	age INTEGER NOT NULL,
+DB name 'koala_holla'
+
+CREATE TABLE koalas(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (250) NOT NULL,
+    gender VARCHAR (100) NOT NULL,
+    age INTEGER NOT NULL,
     ready_to_transfer BOOLEAN NOT NULL,
     notes VARCHAR (250)
 );
+
+INSERT INTO koalas( name, gender, age, ready_to_transfer, notes)
+VALUES ('Jean', 'female', '5', 'true', 'allergic to lots of lava');
